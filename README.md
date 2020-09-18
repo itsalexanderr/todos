bd746a8350db537cbbffd9affb757ee333b03655
Alexander <arojas@miransdata.com.pe>
2020-09-17 20:34:01 -0500

    primer todo

---
+++ README.md
@@ -0,0 +1,14 @@
+#Todos Inicial
+
+Este es el proyecto inicial para crear aplicaciones utilizando webpack.
+
+### Notas:
+Recuerden reconstruir los mudulos de node.
+````
+npm instal 
+````
+
+Y para construil el buid, recuerden: 
+````
+npm run build
+````
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './styles.css';
+
+import {Todo, TodoList} from './classes';
+import { crearTodoHtml } from './js/componentes';
+// import { Todo } from './classes/todo.class.js'; 
+// import { TodoList } from './classes/todo-list.class';
+
+export const todoList = new TodoList();
+
+// todoList.todos.forEach(crearTodoHtml;
+todoList.todos.forEach(todo => crearTodoHtml( todo ));
